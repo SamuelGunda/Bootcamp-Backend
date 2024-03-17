6ffae2018cf7b22f3a3393f393049b03c3477a55
Samuel Gunda <Samuel.gunda.sg@gmail.com>
2024-03-18 00:26:39 +0100

    login addition

--- src/main/java/com/kasv/gunda/bootcamp/controllers/UserController.java
+++ src/main/java/com/kasv/gunda/bootcamp/controllers/UserController.java
@@ -1,10 +1,10 @@
 package com.kasv.gunda.bootcamp.controllers;
 
+import com.google.gson.Gson;
+import com.kasv.gunda.bootcamp.entities.User;
 import com.kasv.gunda.bootcamp.repositories.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -24,5 +24,25 @@ public class UserController {
             return "User with username " + username + " does not exist";
         }
 
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+
+        if (user.getUsername() == null || user.getPassword() == null) {
+            return ResponseEntity.status(400).body("Username and password are required");
+        }
+
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            if (userFromDb.getPassword().equals(user.getPassword())) {
+                return ResponseEntity.ok("Login successful");
+            } else {
+                return ResponseEntity.status(401).body("Invalid password");
+            }
+        } else {
+            return ResponseEntity.status(404).body("User not found");
+        }
     }
 }
